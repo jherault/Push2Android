f28a83d7fa5a7da1a7cfdbb1982eac5e27b05adc
Jeremy Herault <jeremy.herault@gmail.com>
2011-06-20 08:56:07 +0200

    UI try

--- android/src/org/push2android/Push2AndroidActivity.java
+++ android/src/org/push2android/Push2AndroidActivity.java
@@ -1,15 +1,71 @@
 package org.push2android;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class Push2AndroidActivity extends Activity
-{
-    /** Called when the activity is first created. */
+import java.util.ArrayList;
+
+public class Push2AndroidActivity extends Activity {
+
+    private int mScreenId = -1;
+    private int mAccountSelectedPosition = 0;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setScreenContent(R.layout.main);
+    }
+
+    private void setScreenContent(int screenId) {
+        mScreenId = screenId;
+        setContentView(screenId);
+        switch (screenId) {
+            // Screen shown if phone is registered/connected
+            case R.layout.main: {
+                setMainScreenContent();
+                break;
+            }
+        }
+    }
+
+    private void setMainScreenContent() {
+
+        // Display accounts
+        String accounts[] = getGoogleAccounts();
+        if (accounts.length == 0) {
+            TextView promptText = (TextView) findViewById(R.id.select_text);
+            promptText.setText(R.string.no_accounts);
+            TextView nextText = (TextView) findViewById(R.id.click_next_text);
+            nextText.setVisibility(TextView.INVISIBLE);
+            //nextButton.setEnabled(false);
+        } else {
+            ListView listView = (ListView) findViewById(R.id.select_account);
+            listView.setAdapter(new ArrayAdapter<String>(this,
+                    R.layout.account, accounts));
+            listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+            listView.setItemChecked(mAccountSelectedPosition, true);
+        }
+    }
+
+     private String[] getGoogleAccounts() {
+        ArrayList<String> accountNames = new ArrayList<String>();
+        Account[] accounts = AccountManager.get(this).getAccounts();
+        for (Account account : accounts) {
+            if (account.type.equals("com.google")) {
+                accountNames.add(account.name);
+            }
+        }
+
+        String[] result = new String[accountNames.size()];
+        accountNames.toArray(result);
+        return result;
     }
 }
